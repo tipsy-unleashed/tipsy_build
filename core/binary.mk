bb77c3bfcd8621c9937e4632051a42e06de32393
martinusbe <martinusbe@gmail.com>
2015-08-23 13:34:41 +0200

    Revert "Pulse OPTIMIZATIONS V1"
    This reverts commit 6e7a05ad2d1b4f5574750b74235d1879b951eaa7.

--- core/binary.mk
+++ core/binary.mk
@@ -97,10 +97,6 @@ else
   endif
 endif
 
-ifeq ($(PULSE),true)
-include $(BUILD_SYSTEM)/graphite.mk
-endif
-
 # The following LOCAL_ variables will be modified in this file.
 # Because the same LOCAL_ variables may be used to define modules for both 1st arch and 2nd arch,
 # we can't modify them in place.
