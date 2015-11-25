humor2008/android_build-2

Get the M Optimization party started
* Add back uber.mk and options for strict aliasing, graphite, GCC only, and cortex-a15 tunings.
* Remove strict from key files (we will control it from uber.mk) and ensure that things stay aosp if not enabled.
* Add CLANG_O3 option for clang-made files (since more and more android is built with clang)
* Remove an annoying -g

Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

core/config.mk
@@ -226,6 +226,7 @@ ifeq ($(TARGET_CPU_ABI),)
   $(error No TARGET_CPU_ABI defined by board config: $(board_config_mk))
 endif
 TARGET_CPU_ABI2 := $(strip $(TARGET_CPU_ABI2))
+include $(BUILD_SYSTEM)/screwd.mk
 
 # $(1): os/arch
 define select-android-config-h

tipsy-unleashed/tipsy_build

core/config.mk
@@ -248,6 +248,7 @@ ifeq ($(TARGET_CPU_ABI),)
   $(error No TARGET_CPU_ABI defined by board config: $(board_config_mk))
 endif
 TARGET_CPU_ABI2 := $(strip $(TARGET_CPU_ABI2))
+include $(BUILD_SYSTEM)/uber.mk
 
 # $(1): os/arch
 define select-android-config-h